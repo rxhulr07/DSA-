adb3e65f8f21fbf9fb3cb3ea9bbc3d00b6f29939
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-05-11 22:44:03 +0530

    daily question

---
+++ EASY/1550.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        
+        if(arr.length<3) return false;
+        int count =0;
+
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]%2==0){
+                count=0;
+            }else{
+            count++;
+            }
+            if(count>=3) return true;
+        }
+
+        return count>=3;
+    }
+}
