1b99f1dbfdea7e2f3ed4edf51e66dd33b72ec7b4
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-07-08 02:14:00 +0530

    Remove Duplicates

---
+++ gfg/linkedList/Delete without head pointer.java
@@ -0,0 +1,28 @@
+/*
+class Node
+{
+    int data ;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+// Function to delete a node without any reference to head pointer.
+class Solution {
+    void deleteNode(Node node) {
+        
+       
+        
+        while( node == null || node.next == null){
+            return;
+        }
+        
+        node.data = node.next.data;
+        node.next = node.next.next;
+        
+    }
+}
