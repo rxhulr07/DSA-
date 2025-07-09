4455824bc2e9f06cab1763dadc9249e0f5cc1beb
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-07-10 01:34:45 +0530

    reverse linked list

---
+++ gfg/linkedList/Reverse a linked list.java
@@ -0,0 +1,19 @@
+// function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+       
+    }
+}
