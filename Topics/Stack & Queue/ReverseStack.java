0594e9b514dabe14e53b0b36608304a8ade7784a
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-08-12 00:48:25 +0530

    reverse stack

--- Topics/Stack & Queue/ReverseStack.java
+++ Topics/Stack & Queue/ReverseStack.java
@@ -1,34 +1,39 @@
 import java.util.*;
 
 public class ReverseStack {
-    
 
-    public static void ReverseStack( Stack <Integer> s){
-        if(s.isEmpty()){
-            s.push(top);
+    // Push an element at the bottom of the stack
+    public static void pushAtBottom(Stack<Integer> s, int value) {
+        if (s.isEmpty()) {
+            s.push(value);
             return;
         }
         int top = s.pop();
-        ReverseStack( s);
+        pushAtBottom(s, value);
         s.push(top);
+    }
 
+    // Reverse the stack using recursion
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
     }
 
     public static void main(String[] args) {
-        Stack <Integer> s = new Stack<>();
+        Stack<Integer> s = new Stack<>();
 
         s.push(1);
         s.push(3);
         s.push(4);
 
-        ReverseStack( 5, s);
-        while(!s.isEmpty()){
-            System.out.println(s.peek());
-            s.pop();
-        }
-        
-    }
-    
-}
+        reverseStack(s);
 
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
 }
