4fa303ac734c1ff8ae5a60bc906865927dd1157c
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-08-15 02:21:43 +0530

    queue using linkedlist

---
+++ Topics/Stack & Queue/QueueLs.java
@@ -0,0 +1,64 @@
+public class QueueLs {
+
+    static class Node{
+        int data;
+        Node next;
+
+         Node(int data){
+            this.data = data;
+             next = null;  
+        }
+    }
+
+        static Node head = null;
+        static Node tail = null;
+      
+        public static boolean isEmpty(){
+            return head == null && tail == null;
+
+        }
+
+        public static void add(int data){
+            Node newNode = new Node(data);
+           if(tail== null){
+            tail=head=newNode;
+            return;
+           }
+           tail.next = newNode;
+           tail = newNode;
+
+        }
+
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int result = head.data;
+            if(head==tail){
+                result = head.data;
+                head = tail =null;
+                return result;
+            }
+            head = head.next;
+            return result;
+
+        }
+    
+
+        public static void main(String[] args) {
+            QueueLs q = new QueueLs();
+            q.add(3);
+            q.add(4);
+            q.add(5);
+            q.add(6);
+            q.remove();
+            Node temp = head;
+            while(temp!= null){
+                System.out.println(temp.data);
+                temp = temp.next; 
+            }
+            
+        }
+    
+}
